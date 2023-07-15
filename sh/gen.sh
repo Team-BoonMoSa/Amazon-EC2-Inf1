f18d76c3f1c910c2044266f59d9923a3914ca84f
Zerohertz <ohg3417@gmail.com>
2023-07-15 20:30:23 +0900

    :hammer: Fix: Triton Inference Server

--- sh/gen.sh
+++ sh/gen.sh
@@ -1,7 +1,7 @@
 python3 server/model-repository/gen_triton_model.py \
     --model_type pytorch \
-    --triton_input images,TYPE_FP32,1x3x640x640 \
-    --triton_output output0,TYPE_FP32,1x25200x38 output1,TYPE_FP32,1x32x160x160 \
-    --compiled_model server/model-repository/BoonMoSa/1/model_neuron.pt \
-    --neuron_core_range 0:3 \
-    --triton_model_dir BoonMoSa
+    --triton_input images__0,FP32,1x3x640x640 \
+    --triton_output output__0,FP32,1x25200x38 output__1,FP32,1x32x160x160 \
+    --compiled_model /model-repository/BoonMoSa/1/model_neuron.pt \
+    --neuron_core_range 0:0 \
+    --triton_model_dir server/model-repository/BoonMoSa
